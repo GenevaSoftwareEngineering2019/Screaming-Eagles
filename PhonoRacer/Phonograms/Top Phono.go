95b526f25860863ffe97846ad6647c57c66d2178
Colton <cwkalmar@geneva.edu>
2018-11-15 21:34:19 -0500

    Rough Phono

---
+++ PhonoRacer/Phonograms/Top Phono.go
@@ -0,0 +1,35 @@
+components {
+  id: "Top Phono"
+  component: "/Phonograms/Top Phono.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/Phonograms/Phono Pictures/Phonograms.atlas\"\n"
+  "default_animation: \"Yellow Box\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 510.0
+    y: 364.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
