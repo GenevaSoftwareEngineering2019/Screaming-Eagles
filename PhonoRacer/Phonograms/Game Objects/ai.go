eeb62a3696cc06b5be64f9eff43da49bb719ad22
Nathan Moelker <njmoelke@geneva.edu>
2018-12-03 16:16:11 -0500

    Beginning Audio

--- PhonoRacer/Phonograms/Game Objects/ai.go
+++ PhonoRacer/Phonograms/Game Objects/ai.go
@@ -52,3 +52,23 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "sound"
+  type: "sound"
+  data: "sound: \"/Phonograms/Phonogram Audio/01.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
