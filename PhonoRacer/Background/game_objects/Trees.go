6f1f7d1e167501698d93e1888e0525a3123887cf
unknown <njmoelke@geneva.edu>
2019-02-05 20:14:20 -0500

    Fixed the Trees

---
+++ PhonoRacer/Background/game_objects/Trees.go
@@ -0,0 +1,55 @@
+components {
+  id: "Trees"
+  component: "/Background/Trees.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/Background/background.atlas\"\n"
+  "default_animation: \"Tree(Resized)\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 291.0
+    y: 335.0
+    z: 0.6
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite2"
+  type: "sprite"
+  data: "tile_set: \"/Background/background.atlas\"\n"
+  "default_animation: \"Tree(Resized)\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 1056.0
+    y: 335.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
