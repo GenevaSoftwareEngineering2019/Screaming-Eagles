23eb380281202a59fcbbcf7d5061c03c97d74c14
Colton <cwkalmar@geneva.edu>
2019-02-19 20:17:29 -0500

    Gamescreen Menu Stuff

---
+++ PhonoRacer/GameScreen/Gamescreen Menu.go
@@ -0,0 +1,15 @@
+components {
+  id: "GamescreenMenu"
+  component: "/GameScreen/GamescreenMenu.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
