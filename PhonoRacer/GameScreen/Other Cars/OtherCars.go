b2869e7ece7ad9ed1f40ef1eac2a1f5a488c5d53
Colton <cwkalmar@geneva.edu>
2019-03-19 14:09:11 -0400

    Car animations and some image cleaning

--- PhonoRacer/GameScreen/Other Cars/OtherCars.go
+++ PhonoRacer/GameScreen/Other Cars/OtherCars.go
@@ -16,8 +16,8 @@ components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/Background/bricks.atlas\"\n"
-  "default_animation: \"RaceCar\"\n"
+  data: "tile_set: \"/GameScreen/Other Cars/OtherCars.atlas\"\n"
+  "default_animation: \"OtherCar1\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
@@ -71,6 +71,46 @@ embedded_components {
   position {
     x: 0.0
     y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite1"
+  type: "sprite"
+  data: "tile_set: \"/GameScreen/Other Cars/OtherCars.atlas\"\n"
+  "default_animation: \"OtherCar1Wheels\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 171.0
+    y: -42.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite2"
+  type: "sprite"
+  data: "tile_set: \"/GameScreen/Other Cars/OtherCars.atlas\"\n"
+  "default_animation: \"OtherCar1Wheels\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: -146.0
+    y: -40.0
     z: 0.0
   }
   rotation {
--- PhonoRacer/GameScreen/car/Car1.go
+++ PhonoRacer/GameScreen/car/Car1.go
@@ -17,13 +17,53 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/GameScreen/car/Car1.atlas\"\n"
-  "default_animation: \"Car\"\n"
+  "default_animation: \"UserCar\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
   position {
     x: 0.0
     y: 0.0
+    z: 1.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite1"
+  type: "sprite"
+  data: "tile_set: \"/GameScreen/car/Car1.atlas\"\n"
+  "default_animation: \"Wheels\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 222.0
+    y: -60.0
+    z: 1.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite2"
+  type: "sprite"
+  data: "tile_set: \"/GameScreen/car/Car1.atlas\"\n"
+  "default_animation: \"Wheels\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: -258.0
+    y: -60.0
     z: 1.0
   }
   rotation {
