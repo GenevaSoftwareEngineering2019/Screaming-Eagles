df95e9806d290466b1e2f7d13ed3387e2b4489ed
Colton <cwkalmar@geneva.edu>
2019-03-30 10:51:39 -0400

    Basic RecapScreen Implementation Started

---
+++ PhonoRacer/GameScreen/Endscreen Gui/Recap.go
@@ -0,0 +1,15 @@
+components {
+  id: "Recap"
+  component: "/GameScreen/Endscreen Gui/Recap.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
