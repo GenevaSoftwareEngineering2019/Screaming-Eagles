9d12f7f85e87dae3cc041fd6d3918271149827b8
Colton <cwkalmar@geneva.edu>
2019-04-16 20:01:28 -0400

    Finishline Work

---
+++ PhonoRacer/GameScreen/Finishline/Finishline.go
@@ -0,0 +1,55 @@
+components {
+  id: "Finishline"
+  component: "/GameScreen/Finishline/Finishline.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/GameScreen/Endscreen Gui/EndScreen.atlas\"\n"
+  "default_animation: \"FinishLine2\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 1.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite1"
+  type: "sprite"
+  data: "tile_set: \"/GameScreen/Endscreen Gui/EndScreen.atlas\"\n"
+  "default_animation: \"FinishLineInverse\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 109.0
+    z: 0.5
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
